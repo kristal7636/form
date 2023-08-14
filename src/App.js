7159c3b8d19020f3dd44eb13bfad682323f20f7b
kristal <kristalpatel86@gmail.com>
2023-08-14 17:29:19 +0530

    add this fil

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +17,19 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      {/* <h1>kristal1</h1>
+      <h1>kristal2</h2>
+      <h1>kristal1</h1>
+      <h1>kristal1</h1>
+      <h1>kristal1</h1> */}
+
+      <form><label>
+        Name:
+        <input type="text" name="name"/>
+        </label>
+        <input type="submit" value="submit"/>
+      </form>
     </div>
   );
 }
